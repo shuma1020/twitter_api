55d9fa34ca5b1f9f4124826d3aede4afc0e6acb4
shuma1020 <shumaseki40@gmail.com>
2020-01-27 16:55:27 +0900

    twitter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,14 +4,16 @@ module ApplicationHelper
     if post.present?
       if post.id.present?
         twitter_card[:url] = "https://iamrecruitme.herokuapp.com/posts/#{post.id}"
-        twitter_card[:image] = "https://s3-ap-northeast-1.amazonaws.com/[アプリ名]-production/images/#{post.id}.png"
+        twitter_card[:image] = "https://s3-ap-northeast-1.amazonaws.com/i-am-production/images/#{post.id}.png"
       else
+        p "kkkk"
         twitter_card[:url] = 'https://iamrecruitme.herokuapp.com/'
-        twitter_card[:image] = "https://raw.githubusercontent.com/[GitHubのユーザーID]/[アプリ名]/master/app/assets/images/top.png"
+        twitter_card[:image] = "https://raw.githubusercontent.com/shuma1020/twitter-api/master/app/assets/images/top.png"
       end
     else
+      p "kkkk"
       twitter_card[:url] = 'https://iamrecruitme.herokuapp.com/'
-      twitter_card[:image] = "https://raw.githubusercontent.com/[GitHubのユーザーID]/[アプリ名]/master/app/assets/images/top.png"
+      twitter_card[:image] = "https://raw.githubusercontent.com/shuma1020/twitter-api/master/app/assets/images/top.png"
     end
     twitter_card[:title] = "画像下に表示されるタイトル"
     twitter_card[:card] = 'summary_large_image'
--- app/views/posts/_post.json.jbuilder
+++ app/views/posts/_post.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! post, :id, :content, :picture, :hash, :created_at, :updated_at
+json.extract! post, :id, :content, :picture, :kind, :created_at, :updated_at
 json.url post_url(post, format: :json)
