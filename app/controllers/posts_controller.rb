b2d7fd3936291d92bc90305fb2d303fc685b4f6b
shuma1020 <shumaseki40@gmail.com>
2020-02-06 14:42:12 +0900

    fix a bit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -88,7 +88,7 @@ class PostsController < ApplicationController
 
     def set_user
       if session[:user_id]
-      @user = User.find(session[:user_id])
+        @user = User.find(session[:user_id])
       end
     end
 
