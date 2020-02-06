8556b862be6338e8d11e8624732764c428a12206
shuma1020 <shumaseki40@gmail.com>
2020-02-06 14:17:15 +0900

    test

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,13 +2,10 @@ module ApplicationHelper
   def get_twitter_card_info(post)
     twitter_card = {}
     if post.present?
-      if post.id.present?
+
         twitter_card[:url] = "https://iamrecruitme.herokuapp.com/posts/#{post.id}"
         twitter_card[:image] = "https://s3-ap-northeast-1.amazonaws.com/i-am-production/images/#{post.id}.png"
-      else
-        twitter_card[:url] = 'https://iamrecruitme.herokuapp.com/'
-        twitter_card[:image] = "https://raw.githubusercontent.com/shuma1020/twitter-api/master/app/assets/images/top.png"
-      end
+
     else
       twitter_card[:url] = 'https://iamrecruitme.herokuapp.com/'
       twitter_card[:image] = "https://raw.githubusercontent.com/shuma1020/twitter-api/master/app/assets/images/top.png"
