aa33722cf3a2900b8f9a587162b5760f15179b6d
shuma1020 <shumaseki40@gmail.com>
2020-02-16 19:45:00 +0900

    mouseover

--- app/javascript/demo_js.js
+++ app/javascript/demo_js.js
@@ -1,3 +1,16 @@
-$(document).ready(function(){
-  $("p").css("color","green");
+$(function() {
+
+  //マウスを乗せたら発動
+  $('.header-title').mouseover(function() {
+
+    //マウスを乗せたら色が変わる
+    $(this).animate({fontSize : '36px'}, 500);
+
+  //ここにはマウスを離したときの動作を記述
+  }).mouseout(function() {
+
+    //アニメーションしながらテキストサイズが元のサイズに戻る
+    $(this).animate({fontSize : '24px'}, 500);
+
   });
+});
