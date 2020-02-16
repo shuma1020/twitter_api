30d06dda714e67972dedcee4359bcdcdf5fdf472
shuma1020 <shumaseki40@gmail.com>
2020-02-16 18:55:21 +0900

    turns green by jq

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require('jquery')
+require ('demo_js.js')
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -16,3 +17,6 @@ require('jquery')
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+//= require jquery
+//= require jquery_ujs
+//= require_tree .
