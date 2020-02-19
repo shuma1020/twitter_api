0db49d3f6e1099d104eed8493fbb91eba68ef16f
shuma1020 <shumaseki40@gmail.com>
2020-02-19 10:37:50 +0900

    card

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,15 +7,15 @@ module ApplicationHelper
         twitter_card[:image] = "https://s3-ap-northeast-1.amazonaws.com/i-am-production/images/#{post.id}.png"
       else
         twitter_card[:url] = 'https://iamrecruitme.herokuapp.com/'
-        twitter_card[:image] = "https://s3-ap-northeast-1.amazonaws.com/i-am-production/images/54.png"
+        <meta property="og:image" content=Rails.root.join("app/assets/images/top.png") />
       end
     else
       twitter_card[:url] = 'https://iamrecruitme.herokuapp.com/'
-      twitter_card[:image] = "https://s3-ap-northeast-1.amazonaws.com/i-am-production/images/54.png"
+      <meta property="og:image" content=Rails.root.join("app/assets/images/top.png") />
     end
-    twitter_card[:title] = "Twitterで逆求人　I-AM"
+    twitter_card[:title] = "雰囲気で繋がる"
     twitter_card[:card] = 'summary_large_image'
-    twitter_card[:description] = 'I-AMは自分のスキルなどをTwitterで発信して逆求人の手助けを行うサービスです'
+    twitter_card[:description] = '自分の雰囲気をシェアしてTwitterで新たな出会いを応援するサービスです'
     twitter_card
   end
 end
